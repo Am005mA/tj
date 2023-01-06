db4cc5f4f3cc25c5ed519637a16e1cb1668896a9
Am005mA <117369749+Am005mA@users.noreply.github.com>
2023-01-06 13:39:33 +0330

    Update systemrestart.py

--- systemrestart.py
+++ systemrestart.py
@@ -9,7 +9,7 @@ def get_json_data(path):
         data_dict = json.load(_json_file_)
     return data_dict
 
-user_data = get_json_data(json)
+user_data = get_json_data(json_path)
 for _uuid_ in user_data:
     subprocess.run(['trojan-go', '-api-addr', '127.0.0.1:10000', '-api', 'set', '-add-profile', '-target-password', _uuid_])
     subprocess.run(['trojan-go', '-api-addr', '127.0.0.1:10000', '-api', 'set', '-modify-profile', '-target-password', _uuid_, '-ip-limit', user_data[_uuid_]["user_limit"]])
